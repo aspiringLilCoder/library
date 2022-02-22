e5e89b7ba75ca9a7ef03c08664c439b1ef59623a
aspiringLilCoder <aspiringlilcoder@gmail.com>
2022-02-22 11:49:17 +0800

    classified..?

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const addBook = document.querySelector("#submit");
 const archive = document.querySelector("#archive"); 
 
 
-
-function Book(title, author, pages, read) {
+class Book {
+  constructor(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  }
+  
 }
 
 
